94d2d8117b9d5ac653e8895651f6ef76e297e586
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-04-01 23:46:07 +0500

    creat CLI Todo app

--- todo.js
+++ todo.js
@@ -1,100 +1,116 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-let storeData = [];
-let condition = true;
-console.log(chalk.bgGreen.white.bold("\n\t\tWELCOM TO TODO APPLICATION\n"));
-let add = await inquirer.prompt([
-    {
-        name: "Add",
-        type: "input",
-        message: chalk.magenta("What do you want to add in your todo list? enter pleas."),
-    },
-]);
-storeData.push(add.Add);
-console.log(chalk.green(storeData), chalk.yellow("Successfully add in your todo list"));
-let again = true;
-while (again) {
-    let moreAdd = await inquirer.prompt([
+async function todoApp() {
+    let storeData = [];
+    let condition = true;
+    console.log(chalk.bgGreen.white.bold("\n\t\tWELCOM TO TODO APPLICATION\n"));
+    let add = await inquirer.prompt([
         {
-            name: "addMore",
-            type: "confirm",
-            message: chalk.magenta("Do you want to add more!?"),
-            default: false,
-        },
-    ]);
-    //again = moreAdd.addMore;
-    let Add = await inquirer.prompt([
-        {
-            name: "add",
+            name: "Add",
             type: "input",
-            message: chalk.blue("Add Item..."),
+            message: chalk.magenta("What do you want to add in your todo list? enter pleas."),
         },
     ]);
-    storeData.push(Add.add);
-    console.log(chalk.green(storeData), chalk.yellow("add successfuly."));
-    again = moreAdd.addMore;
-}
-let Select = await inquirer.prompt([
-    {
-        name: "select",
-        type: "list",
-        message: chalk.magenta(" Select an operation that you want to do in your todo list"),
-        choices: [
-            chalk.yellow("update"),
-            chalk.red("view"),
-            chalk.green("delete"),
-            chalk.blue("exit"),
-        ],
-    },
-]);
-if (Select.select == chalk.yellow("update")) {
-    while (condition) {
-        let update = await inquirer.prompt([
-            {
-                name: "Update",
-                type: "list",
-                message: chalk.magenta("Select an item that you want to update"),
-                choices: storeData.map((val) => val),
-            },
-        ]);
-        let addtodo = await inquirer.prompt([
-            {
-                name: "Add",
-                type: "input",
-                message: chalk.magenta("Add Item..."),
-            },
-        ]);
-        let newTodo = storeData.filter((val) => val !== update.Update);
-        storeData = [...newTodo, addtodo.Add];
-        console.log(chalk.blue(storeData));
+    storeData.push(add.Add);
+    console.log(chalk.green(storeData), chalk.yellow("Successfully add in your todo list"));
+    let again = true;
+    while (again) {
         let moreAdd = await inquirer.prompt([
             {
                 name: "addMore",
                 type: "confirm",
-                message: chalk.magenta("Do you want to update more!?"),
+                message: chalk.magenta("Do you want to add more!?"),
                 default: false,
             },
         ]);
-        condition = moreAdd.addMore;
+        if (!moreAdd.addMore) {
+            again = false;
+            continue;
+        }
+        let Add = await inquirer.prompt([
+            {
+                name: "add",
+                type: "input",
+                message: chalk.blue("Add Item..."),
+            },
+        ]);
+        storeData.push(Add.add);
+        console.log(chalk.green(storeData), chalk.yellow("add successfuly."));
     }
-}
-else if (Select.select == chalk.red("view")) {
-    console.log(chalk.cyan(storeData));
-}
-else if (Select.select == chalk.green("delete")) {
-    let delet = await inquirer.prompt([
+    let Select = await inquirer.prompt([
         {
-            name: "Delet",
+            name: "select",
             type: "list",
-            message: chalk.magenta("what do you want to delet choise pleas."),
-            choices: storeData.map((val) => val),
+            message: chalk.magenta(" Select an operation that you want to do in your todo list"),
+            choices: [
+                chalk.yellow("update"),
+                chalk.red("view"),
+                chalk.green("delete"),
+                chalk.blue("exit"),
+            ],
         },
     ]);
-    let Delet = storeData.filter((val) => val !== delet.Delet);
-    storeData = [...Delet];
-    console.log(chalk.red(storeData));
+    if (Select.select == chalk.yellow("update")) {
+        while (condition) {
+            let update = await inquirer.prompt([
+                {
+                    name: "Update",
+                    type: "list",
+                    message: chalk.magenta("Select an item that you want to update"),
+                    choices: storeData,
+                },
+            ]);
+            let addtodo = await inquirer.prompt([
+                {
+                    name: "Add",
+                    type: "input",
+                    message: chalk.magenta("Add Item..."),
+                },
+            ]);
+            let newTodo = storeData.filter((val) => val !== update.Update);
+            storeData = [...newTodo, addtodo.Add];
+            console.log(chalk.blue(storeData), chalk.yellow.bold("Update Successfully.."));
+            let moreAdd = await inquirer.prompt([
+                {
+                    name: "addMore",
+                    type: "confirm",
+                    message: chalk.magenta("Do you want to update more!?"),
+                    default: false,
+                },
+            ]);
+            condition = moreAdd.addMore;
+        }
+    }
+    else if (Select.select == chalk.red("view")) {
+        console.log(chalk.cyan(storeData));
+    }
+    else if (Select.select == chalk.green("delete")) {
+        let delet = await inquirer.prompt([
+            {
+                name: "Delet",
+                type: "list",
+                message: chalk.magenta("what do you want to delet choise pleas."),
+                choices: storeData,
+            },
+        ]);
+        let Delet = storeData.filter((val) => val !== delet.Delet);
+        storeData = [...Delet];
+        console.log(chalk.red(storeData), chalk.yellow.bold("Successfully Delet from your todo..."));
+    }
+    else if (Select.select == chalk.blue("exit")) {
+        console.log(chalk.blue("Exit"));
+    }
+    let Another = await inquirer.prompt([
+        {
+            message: chalk.magenta.bold("Do you Want To do again Run todo app ?"),
+            type: "list",
+            name: "Again",
+            choices: ["Yes", "No"],
+        },
+    ]);
+    if (Another.Again === "Yes") {
+        todoApp();
+    }
 }
-else if (Select.select == chalk.blue("exit")) {
-    console.log(chalk.blue("Exit"));
-}
+todoApp();
